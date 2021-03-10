99ef257a636aa6ec1ac79255474016af979c8dac
sungil <abcd8383@naver.com>
2021-03-10 15:58:38 +0900

    최종커밋

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 struct tel{
 	char[20] tel;
@@ -61,17 +62,49 @@ void UpdateTel(){
 			arr[i].name=name;
 			printf("정상적으로 수정되었습니다.");
 			return;			
-			}
 		}
+		
 	}
 
 }
 
 void SearchTel(){
 	if(index==0){
+		printf("현재 등록된 전화부가 없습니다");
+	return;		
+	}
+	char name[20];
+	printf("검색할 이름 입력");
+	scanr("%s",name);
+		for(i=0; i<index; i++){
+		if(strcmp(arr[i].name,name)==0){
+			
+			printf("전화번호:%s",arr[i].tel);
+			return;			
+		}
 		
 	}
+	
 }
 void main(){
-
+	int num;
+	while(true){
+		printf("원하는 메뉴 선택");
+		printf("1.등록, 2. 검색, 3. 삭제, 4. 수정");
+		scanf("%d",&num);
+		switch(num){
+			case 1:
+			AddTel();
+			break;
+			case 2:
+			SearchTel();
+			break;
+			case 3:
+			DeleteTel();
+			break;
+			case 4:
+			UpdateTel();
+			break;
+		}
+	}
 }
